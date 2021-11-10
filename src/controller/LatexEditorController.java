158331eddf2c031affa9aa147d3b24a3aa07882f
sotblad <sotiris2009@hotmail.com>
2021-11-10 12:58:41 +0200

    Refactoring task 1

--- src/controller/LatexEditorController.java
+++ src/controller/LatexEditorController.java
@@ -1,18 +1,11 @@
 package controller;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
-import controller.commands.AddLatexCommand;
-import controller.commands.ChangeVersionsStrategyCommand;
 import controller.commands.Command;
 import controller.commands.CommandFactory;
-import controller.commands.CreateCommand;
-import controller.commands.DisableVersionsManagementCommand;
-import controller.commands.EditCommand;
-import controller.commands.EnableVersionsManagementCommand;
-import controller.commands.LoadCommand;
-import controller.commands.RollbackToPreviousVersionCommand;
-import controller.commands.SaveCommand;
 import model.VersionsManager;
 
 public class LatexEditorController{
@@ -20,17 +13,25 @@ public class LatexEditorController{
 	
 	public LatexEditorController(VersionsManager versionsManager) {
 		CommandFactory commandFactory = new CommandFactory(versionsManager);
+		commands = new HashMap<String, Command>();
 		
-		commands = new HashMap<String, Command>(); 
-		commands.put("addLatex", commandFactory.createCommand("addLatex"));
-		commands.put("changeVersionsStrategy", commandFactory.createCommand("changeVersionsStrategy"));
-		commands.put("create", commandFactory.createCommand("create"));
-		commands.put("disableVersionsManagement", commandFactory.createCommand("disableVersionsManagement"));
-		commands.put("edit", commandFactory.createCommand("edit"));
-		commands.put("enableVersionsManagement", commandFactory.createCommand("enableVersionsManagement"));
-		commands.put("load", commandFactory.createCommand("load"));
-		commands.put("rollbackToPreviousVersion", commandFactory.createCommand("rollbackToPreviousVersion"));
-		commands.put("save", commandFactory.createCommand("save"));
+		List<String> commandsList = Arrays.asList(
+				new String[] {
+						"addLatex",
+						"changeVersionsStrategy",
+						"create",
+						"disableVersionsManagement",
+						"edit",
+						"enableVersionsManagement",
+						"load",
+						"rollbackToPreviousVersion",
+						"save"
+				}
+		);
+
+		for(int i = 0; i < commandsList.size(); i++) {
+			commands.put(commandsList.get(i), commandFactory.createCommand(commandsList.get(i)));
+		}
 		
 	}
 	
