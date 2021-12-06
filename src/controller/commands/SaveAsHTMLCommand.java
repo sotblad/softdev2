d4c42f8a44655c17d3b9b423426b092f6316901c
sotblad <sotiris2009@hotmail.com>
2021-12-06 05:30:10 +0200

    almost ready latex->html

--- src/controller/commands/SaveAsHTMLCommand.java
+++ src/controller/commands/SaveAsHTMLCommand.java
@@ -20,10 +20,16 @@ public class SaveAsHTMLCommand implements Command {
 	private HashMap<String, String> htmlDocument = new HashMap<String, String>();
 	private ArrayList<String> noClosingTags = new ArrayList<String>();
 	private ArrayList<String> toDelete = new ArrayList<String>();
+	private HashMap<String, String> begins = new HashMap<String, String>();
 	private int titleStart = -1;
 	private int titleEnd = -1;
 	private int ChapterCount;
 	private int sectionCount;
+	private boolean numberingEnabled = true;
+	private String letterSignature;
+	private String letterSignerAddress;
+	private boolean isLetter = false;
+	private String letterDestination;
 	
 	public SaveAsHTMLCommand() {
 		Date date = new Date();
@@ -44,22 +50,26 @@ public class SaveAsHTMLCommand implements Command {
 		LatexHTML.put("\\textbf{text}", "<b>");
 		LatexHTML.put("\\texttt{text}", "<tt>");
 		LatexHTML.put("\\n", "<br>");
-		LatexHTML.put("\\title", "<h2><center>");
+		LatexHTML.put("\\title", "<center><h2>");
 		LatexHTML.put("\\and", "&emsp;");
 		LatexHTML.put("\\date", "<p>");
 		LatexHTML.put("\\today", date.toString());
 		LatexHTML.put("\\author", "<p>");
 		LatexHTML.put("\\chapter", "<h1>");
 		LatexHTML.put("\\chapter*", "<h1>");
-		LatexHTML.put("\\begin", "<body>");
-		LatexHTML.put("\\maketitle", "</h2></center>"); // xwris close
+		LatexHTML.put("\\begin", "<>");
+		LatexHTML.put("\\maketitle", "</h2></center>");
+		LatexHTML.put("\\end", "<>");// xwris close
+		LatexHTML.put("\\encl", "encl: ");// xwris close
+		LatexHTML.put("\\opening", "<div>");// xwris close
+		LatexHTML.put("\\closing", "</div><p style='float: right'>");
+		LatexHTML.put("\\ps", "");
 		
 		noClosingTags.add("\\maketitle");
 		noClosingTags.add("\\title");
-		
-		toDelete.add("\\mainmatter");
-		toDelete.add("\\backmatter");
-		toDelete.add("\\frontmatter");
+		noClosingTags.add("\\encl");
+		noClosingTags.add("\\opening");
+		noClosingTags.add("\\ps");
 	}
 	
 	public String closingTag(String tag) {
@@ -81,13 +91,40 @@ public class SaveAsHTMLCommand implements Command {
 					String extra = "";
 
 					if(FirstPart.equals("\\chapter")) {
-						if(ChapterCount != 0) {
-							extra = "Chapter " + ChapterCount + ": <br>";
+						if(numberingEnabled) {
+							extra += extra = "Chapter "+ ChapterCount + ": <br><br>";
 						}
 						ChapterCount++;
 					}else if(FirstPart.equals("\\section")) {
 						extra = sectionCount + " ";
 						sectionCount++;
+					}else if(FirstPart.equals("\\closing")) {
+						extra = LatexHTML.get(FirstPart) + SecondPart + "<br>" + letterSignature + "</p>";
+						return extra;
+					}else if(FirstPart.equals("\\begin")) {
+						if(SecondPart.equals("document")) {
+							result = "<body>";
+							return result;
+						}else if(SecondPart.equals("abstract")) {
+							result = "<center><h3>Abstract</h3></center>";
+							return result;
+						}else if(SecondPart.equals("letter")) {
+							letterDestination = ThirdPart.substring(ThirdPart.indexOf("{")+1, ThirdPart.indexOf("}")) + "<p>";
+							result = "<div id='letter'>";
+							isLetter = true;
+							return result;
+						}
+					}else if(FirstPart.equals("\\end")) {
+						if(SecondPart.equals("document")) {
+							result = "</body>";
+							return result;
+						}else if(SecondPart.equals("abstract")) {
+							result = "";
+							return result;
+						}else if(SecondPart.equals("letter")) {
+							result = "</div>";
+							return result;
+						}
 					}
 					FirstPart = LatexHTML.get(FirstPart);
 					result = FirstPart + extra + SecondPart + closingTag(FirstPart) + ThirdPart;
@@ -98,17 +135,36 @@ public class SaveAsHTMLCommand implements Command {
 			}else {
 				String fontSize = "";
 				if(line.contains("documentclass")) {
-					fontSize = line.substring(line.indexOf("[")+1, line.indexOf(","));
-					result = "<html><font style='font-size:"+fontSize+"'>";
+					if(line.contains("[")) {
+						fontSize = line.substring(line.indexOf("[")+1, line.indexOf(","));
+						result = "<font style='font-size:"+fontSize+"'>";
+					}
+					return "";
+				}else if(line.contains("usepackage")) {
+					return "";
+				}else if(line.contains("signature")) {
+					letterSignature = SecondPart; 
+					return "";
+				}else if(line.contains("\\address{")) {
+					letterSignerAddress = SecondPart + "<br>"; 
+					return "";
 				}else {				
 					result = FirstPart + SecondPart + ThirdPart;
 				}
 			}
 		}else {
-			for(int i = 0;i<toDelete.size();i++) {
-				if(line.contains(toDelete.get(i))) {
-					return "";
-				}
+			if(line.contains("backmatter")) {
+				numberingEnabled = false;
+				return "";
+			} else if(line.contains("mainmatter")) {
+				ChapterCount = 1;
+				numberingEnabled = true;
+				return "";
+			}else if(line.contains("frontmatter")) {
+				numberingEnabled = false;
+				return "";
+			}else if(line.contains("%") && line.substring(0, 1).equals("%")) {
+				return "<!-- " + line.substring(1) + " -->";
 			}
 			return line;
 		}
@@ -119,8 +175,13 @@ public class SaveAsHTMLCommand implements Command {
 		LatexEditorView latexEditorView = versionsManager.getEditorView();
 		String documentContents = latexEditorView.getCurrentDocument().getContents();
 		String replace = documentContents.replace("\\n", "<br>");
-		ChapterCount = 0;
+		numberingEnabled = true;
+		ChapterCount = 1;
 		sectionCount = 1;
+		letterSignature = "";
+		isLetter = false;
+		letterSignerAddress = "";
+		letterDestination = "";
 		
 		Scanner scanner = new Scanner(replace);
 		String result = "";
@@ -131,6 +192,13 @@ public class SaveAsHTMLCommand implements Command {
 				  result = result.replace(me.getKey().toString(), me.getValue().toString());
 			  }
 		}
+		if(isLetter) {
+			Date date = new Date();
+			result = "<html><p style='float: right'>"+letterSignerAddress+"<br>" + date.toString() + "</p><br><br>" + letterDestination + result;
+		  }else {
+			  result = "<html>" + result;
+		  }
+		result += "</html>";
 		System.out.println(result);
 		scanner.close();
 	}
